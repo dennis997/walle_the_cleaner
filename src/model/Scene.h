8e56daadf7dc96f4469a6325f6fd9acb48ae3dff
Dennis Hilz <dennis.hilz@stud.h-da.de>
2022-03-04 14:11:57 +0100

    [setup] Removed unnecessary include

--- src/model/Scene.h
+++ src/model/Scene.h
@@ -2,6 +2,13 @@
 #define WALL_E_THE_CLEANER_COMPONENT_H
 
 #include <list>
+#ifdef __APPLE__
+/* Defined before OpenGL and GLUT includes to avoid deprecation messages */
+#define GL_SILENCE_DEPRECATION
+#include "/usr/local/include/GL/glew.h"
+#else
+#endif
+
 
 /**
  * A scene is a node in the scene graph.
